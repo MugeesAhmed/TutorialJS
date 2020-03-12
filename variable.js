63916276f6222f22092e967f3c56d0bba9ab7ee5
unknown <mugeesahmed951@gmail.com>
2020-03-12 13:19:06 -0700

    initial commit

---
+++ variable.js
@@ -0,0 +1,11 @@
+//let, const 
+
+let age = 15; //let variable can be modified later in the code
+age = 32; //modifing data of let age variable to 35 from 15
+console.log(age); 
+
+//const 
+const name = "Mugees Ahmed";
+console.log(name);
+
+//always use consts unless you know that you want to reassing value later on
